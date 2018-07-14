28d2d307abe3c6199adabc67decc7b5d58c36162
parth <parthsethi16.ps@gmail.com>
2018-07-14 14:06:32 -0400

    html minify

---
+++ conFusion/js/scripts.js
@@ -0,0 +1,24 @@
+		 $(document).ready(function(){
+            
+            $("#reserveBtn").click(function(){
+                $("#reserveModal").modal();
+            });
+        $("#myBtn").click(function(){
+            $("#myModal").modal();
+            });
+        });
+ 		$(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 1000 } );
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+        });
